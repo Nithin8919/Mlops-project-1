a97e58d5a7d326e82a33cf28425a596d428fae14
Nithin <cherukumallinithin@gmail.com>
2024-06-13 09:08:46 +0530

    edited

--- template.py
+++ template.py
@@ -10,10 +10,10 @@ list_of_files=[
     "src/components/data_transformation.py",
     "src/components/model_trainer.py",
     "src/components/model_evaluation.py",
-    "src/pipeline__init__.py",
+    "src/pipeline/__init__.py",
     "src/pipeline/training_pipeline.py",
     "src/pipeline/prediction_pipeline.py",
-    "src/untils/__init__.py",
+    "src/utils/__init__.py",
     "src/utils/utils.py",
     "src/logger/logging.py",
     "src/exception/exception"
@@ -38,5 +38,3 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass # create an empty file
-
-#its updated
