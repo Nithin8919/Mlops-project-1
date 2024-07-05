dde8cdf529c163066b8d083c1f7edac031a534e9
Nithin <cherukumallinithin@gmail.com>
2024-07-05 15:59:29 +0530

    modular code started

---
+++ src/components/model_evaluation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+from src.logger.app_logging import logging
+from src.exception.exception import customexception
+
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path # to write the path without any issues like mac os and windows / \
+
+@dataclass
+class DataIngestionConfig:
+    pass
+
+class DataOmgestion:
+    def __init__(self):
+        pass
+    
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            logging.info()
+            raise customexception(e,sys)
